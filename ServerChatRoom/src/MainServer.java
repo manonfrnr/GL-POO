8b3ee14cf347139f47150e4751c4e52aef31c92b
Meegan <guillon@et.esiea.fr>
2020-06-05 12:11:52 +0200

    Création du WorkerServer pour la gestion des connexions multiples
    NB : Après avoir run MainServer ouvrir deux terminaux et lancer la commande "telnet localhost 8818" simultanément. Ensuite écrire un msg. Pour quitter écrire "quitter".

--- ServerChatRoom/src/MainServer.java
+++ ServerChatRoom/src/MainServer.java
@@ -14,31 +14,13 @@ public class MainServer {
                 System.out.println("About to accept client connection...");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
-                Thread t = new Thread(){
-                    @Override
-                    public void run(){
-                        try {
-                            gestionClientSocket(clientSocket);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                t.start();
+                WorkerServer workerServer = new WorkerServer(clientSocket);
+                workerServer.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void gestionClientSocket(Socket clientSocket) throws IOException, InterruptedException {
-        OutputStream outputStream = clientSocket.getOutputStream();
-        for(int i=0; i<10; i++){
-            outputStream.write(("Time now is " + new Date() + "\n").getBytes());
-            Thread.sleep(1000);
-        }
-        clientSocket.close();
-    }
+
 }
---
+++ ServerChatRoom/src/WorkerServer.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Date;
+
+public class WorkerServer extends Thread{
+
+    private final Socket clientSocket;
+
+    public WorkerServer(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run (){
+        try {
+            gestionClientSocket();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void gestionClientSocket() throws IOException, InterruptedException {
+        InputStream inputStream = clientSocket.getInputStream();
+        OutputStream outputStream = clientSocket.getOutputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if ("quitter".equalsIgnoreCase(line)){
+                break;
+            }
+            String msg = "tu as ecris: " + line + "\n";
+            outputStream.write(msg.getBytes());
+        }
+        clientSocket.close();
+    }
+}
